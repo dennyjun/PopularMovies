6e721771a16dcc71a8b614d17dea25e6cb5a7f64
Denny <djguitarstyle@gmail.com>
2015-08-14 01:10:31 -0700

    Add trailer table

--- app/src/main/java/com/example/android/popularmovies/providers/MovieContentProvider.java
+++ app/src/main/java/com/example/android/popularmovies/providers/MovieContentProvider.java
@@ -19,16 +19,24 @@ import com.example.android.popularmovies.databases.MovieDbHelper;
 public class MovieContentProvider extends ContentProvider {
     private static final String AUTHORITY = MovieContentProvider.class.getCanonicalName();
     private static final String FAVORITES_BASE_PATH = "favorites";
-    public static final Uri CONTENT_URI =
+    private static final String TRAILERS_BASE_PATH = "trailers";
+
+    public static final Uri FAVORITES_CONTENT_URI =
             Uri.parse("content://" + AUTHORITY + "/" + FAVORITES_BASE_PATH);
+    public static final Uri TRAILERS_CONTENT_URI =
+            Uri.parse("content://" + AUTHORITY + "/" + TRAILERS_BASE_PATH);
 
     private static final int URI_CODE_FAVORITES = 100;
     private static final int URI_CODE_FAVORITE_ID = 110;
+    private static final int URI_CODE_TRAILERS = 120;
+    private static final int URI_CODE_TRAILER_ID = 130;
 
     private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     static {
         uriMatcher.addURI(AUTHORITY, FAVORITES_BASE_PATH, URI_CODE_FAVORITES);                      // returns all favorites
         uriMatcher.addURI(AUTHORITY, FAVORITES_BASE_PATH + "/#", URI_CODE_FAVORITE_ID);             // returns a single favorite movie
+        uriMatcher.addURI(AUTHORITY, TRAILERS_BASE_PATH, URI_CODE_TRAILERS);
+        uriMatcher.addURI(AUTHORITY, TRAILERS_BASE_PATH + "/#", URI_CODE_TRAILER_ID);
     }
 
     private MovieDbHelper movieDbHelper;
@@ -43,24 +51,15 @@ public class MovieContentProvider extends ContentProvider {
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
         final SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
-        queryBuilder.setTables(MovieDbHelper.FAVORITES_TABLE_NAME);
-        final int uriType = uriMatcher.match(uri);
-        switch (uriType) {
-            case URI_CODE_FAVORITES:
-                break;
-            case URI_CODE_FAVORITE_ID:
-                queryBuilder.appendWhere(buildWhereClause(uri));
-                break;
-            default: throw new IllegalArgumentException("Invalid URI: " + uri);
-        }
-        final String sortByTitle = getContext().getString(R.string.moviedb_title_param);
+        queryBuilder.setTables(getTableName(uri));
+        queryBuilder.appendWhere(buildWhereClause(uri));
         final Cursor cursor = queryBuilder.query(
                 movieDbHelper.getReadableDatabase(),
                 projection,
                 selection,
                 selectionArgs,
                 null, null,
-                sortByTitle
+                sortOrder
         );
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
         return cursor;
@@ -74,32 +73,30 @@ public class MovieContentProvider extends ContentProvider {
                 return ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + FAVORITES_BASE_PATH;
             case URI_CODE_FAVORITE_ID:
                 return ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + FAVORITES_BASE_PATH;
+            case URI_CODE_TRAILERS:
+                return ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + TRAILERS_BASE_PATH;
+            case URI_CODE_TRAILER_ID:
+                return ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + TRAILERS_BASE_PATH;
             default: throw new IllegalArgumentException("Invalid URI: " + uri);
         }
     }
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        final int uriType = uriMatcher.match(uri);
-        long id = 0;
-        switch (uriType) {
-            case URI_CODE_FAVORITES:
-                id = movieDbHelper.getWritableDatabase().insertWithOnConflict(
-                        MovieDbHelper.FAVORITES_TABLE_NAME,
-                        null,
-                        values,
-                        SQLiteDatabase.CONFLICT_REPLACE
-                );
-                return ContentUris.withAppendedId(uri, id);
-            default: throw new IllegalArgumentException("Invalid URI: " + uri);
-        }
+        final long id = movieDbHelper.getWritableDatabase().insertWithOnConflict(
+                getTableName(uri),
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE
+        );
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         final String whereClause = buildWhereClause(uri, selection);
         return movieDbHelper.getWritableDatabase().delete(
-                MovieDbHelper.FAVORITES_TABLE_NAME,
+                getTableName(uri),
                 whereClause,
                 selectionArgs
         );
@@ -109,11 +106,22 @@ public class MovieContentProvider extends ContentProvider {
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         final String whereClause = buildWhereClause(uri, selection);
         return movieDbHelper.getWritableDatabase().update(
-                MovieDbHelper.FAVORITES_TABLE_NAME,
+                getTableName(uri),
                 values,
                 whereClause,
                 selectionArgs
         );
+    }
+
+    private String getTableName(final Uri uri) {
+        final int uriType = uriMatcher.match(uri);
+        switch (uriType) {
+            case URI_CODE_FAVORITES:
+                return MovieDbHelper.FAVORITES_TABLE_NAME;
+            case URI_CODE_TRAILERS:
+                return MovieDbHelper.TRAILERS_TABLE_NAME;
+            default: throw new IllegalArgumentException("Invalid URI: " + uri);
+        }
     }
 
     private String buildWhereClause(Uri uri) {
@@ -124,8 +132,10 @@ public class MovieContentProvider extends ContentProvider {
         String whereClause = selection;
         final int uriType = uriMatcher.match(uri);
         switch (uriType) {
+            case URI_CODE_TRAILERS:
             case URI_CODE_FAVORITES:
                 break;
+            case URI_CODE_TRAILER_ID:
             case URI_CODE_FAVORITE_ID:
                 final String idName = getContext().getString(R.string.moviedb_id_param);
                 whereClause = idName + " = " + uri.getLastPathSegment();
