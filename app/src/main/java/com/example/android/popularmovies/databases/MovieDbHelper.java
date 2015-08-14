34dcd474fba62de98800ab78d0e8e5597e83e140
Denny <djguitarstyle@gmail.com>
2015-08-14 00:46:01 -0700

    Move moviedb database name and version from strings.xml to manifest file

--- app/src/main/java/com/example/android/popularmovies/databases/MovieDbHelper.java
+++ app/src/main/java/com/example/android/popularmovies/databases/MovieDbHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.utils.AppUtil;
 import com.example.android.popularmovies.utils.SqlUtil;
 
 /**
@@ -14,7 +15,8 @@ public class MovieDbHelper extends SQLiteOpenHelper {
     public static final String DEF_INT = "INTEGER";
     public static final String DEF_STR = "TEXT";
     public static final String DEF_PRI_KEY = "PRIMARY KEY";
-    public static final String DEF_FOREIGN_KEY = "FOREIGN KEY(%s) REFERENCES %s(%s)";
+    public static final String DEF_FOREIGN_KEY =
+            "FOREIGN KEY(%s) REFERENCES %s(%s) ON DELETE CASCADE";
     public static final String DEF_NOT_NULL = "NOT NULL";
     public static final String DEF_REAL = "REAL";
     public static final String DEF_BLOB = "BLOB";
@@ -22,24 +24,28 @@ public class MovieDbHelper extends SQLiteOpenHelper {
     private final Context context;
 
     public static final String FAVORITES_TABLE_NAME = "favorites";
+    public static final String TRAILERS_TABLE_NAME = "trailers";
+    public static final String REVIEWS_TABLE_NAME = "reviews";
 
     public MovieDbHelper(Context context) {
         super(context,
-                context.getString(R.string.sql_movie_database_name),
+                AppUtil.getMetaData(context, R.string.sql_movie_database_name_meta_data),
                 null,
-                Integer.valueOf(context.getString(R.string.sql_movie_database_version)));
+                Integer.valueOf(AppUtil.getMetaData(context,
+                        R.string.sql_movie_database_version_meta_data)));
         this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         createFavoritesTable(db);
+        createTrailersTable(db);
     }
 
     private void createFavoritesTable(SQLiteDatabase db) {
         db.execSQL(SqlUtil.createTableQuery(FAVORITES_TABLE_NAME,
                 SqlUtil.formatColumn(context.getString(R.string.moviedb_id_param),
-                        DEF_STR, DEF_PRI_KEY),
+                        DEF_STR, DEF_PRI_KEY, DEF_NOT_NULL),
                 SqlUtil.formatColumn(context.getString(R.string.moviedb_adult_param),
                         DEF_INT),
                 SqlUtil.formatColumn(context.getString(R.string.moviedb_original_language_param),
@@ -65,13 +71,34 @@ public class MovieDbHelper extends SQLiteOpenHelper {
         ));
     }
 
+    private void createTrailersTable(SQLiteDatabase db) {
+        db.execSQL(SqlUtil.createTableQuery(TRAILERS_TABLE_NAME,
+                SqlUtil.formatColumn(context.getString(R.string.moviedb_trailer_id_param),
+                        DEF_STR, DEF_PRI_KEY, DEF_NOT_NULL),
+                SqlUtil.formatColumn(
+                        context.getString(R.string.moviedb_trailer_url_sql_column_name),
+                        DEF_STR),
+                SqlUtil.formatColumn(context.getString(R.string.moviedb_id_param),
+                        DEF_STR),
+                String.format(DEF_FOREIGN_KEY,
+                        context.getString(R.string.moviedb_id_param),
+                        FAVORITES_TABLE_NAME,
+                        context.getString(R.string.moviedb_id_param))
+        ));
+    }
+
     private void updateFavoritesTable(SQLiteDatabase db) {
         db.execSQL(SqlUtil.updateTableQuery(FAVORITES_TABLE_NAME));
+    }
+
+    private void updateTrailersTable(SQLiteDatabase db) {
+        db.execSQL(SqlUtil.updateTableQuery(TRAILERS_TABLE_NAME));
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         updateFavoritesTable(db);
+        updateTrailersTable(db);
         onCreate(db);
     }
 }
