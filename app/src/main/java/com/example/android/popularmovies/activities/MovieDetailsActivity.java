836dba909244ed806f0d1f7946a644940bf437d7
Denny <djguitarstyle@gmail.com>
2015-08-14 23:36:40 -0700

    Clean up code

--- app/src/main/java/com/example/android/popularmovies/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/activities/MovieDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.popularmovies.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -26,33 +27,46 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_details);
 
         if(savedInstanceState != null) {
-            final int scrollPosY =
-                    savedInstanceState.getInt(getString(R.string.movie_details_scroll_state_key));
-            final ScrollView scrollView =
-                    (ScrollView) findViewById(R.id.activity_details_scrollview);
-            scrollView.setScrollY(scrollPosY);
+            loadScrollViewState(savedInstanceState);
         }
     }
 
+    private void loadScrollViewState(Bundle savedInstanceState) {
+        final int scrollPosY =
+                savedInstanceState.getInt(getString(R.string.movie_details_scroll_state_key));
+        final ScrollView scrollView =
+                (ScrollView) findViewById(R.id.activity_details_scrollview);
+        scrollView.setScrollY(scrollPosY);
+    }
+
     private void loadFavoriteState() {
-        final Movie movie = new Movie(getApplicationContext(),                                      // super.onCreate will create the fragments so need to store favorite state before that
-                getIntent().getParcelableExtra(Intent.EXTRA_STREAM));
+        final Movie movie = getMovieFromIntent(getApplicationContext());                            // super.onCreate will create the fragments so need to store favorite state before that
+        final boolean favoriteMovie = checkIfFavoriteMovieFromDb(movie.getId());
+        setOriginalFavoriteState(favoriteMovie);                                                    // store original state, need to check when activity is destroyed if need to do work (save / delete fav)
+        setFavoriteMovie(favoriteMovie);
+    }
+
+    private boolean checkIfFavoriteMovieFromDb(final String movieId) {
         final Cursor cursor = getContentResolver().query(
                 MovieContentProvider.FAVORITES_CONTENT_URI,
                 new String[]{getString(R.string.moviedb_id_param)},
                 getString(R.string.moviedb_id_param) + "=?",
-                new String[]{movie.getId()},
+                new String[]{movieId},
                 null);
-        final boolean favoriteMovie = cursor.getCount() != 0;
-        cursor.close();
-
-        getIntent().putExtra("originalFavoriteState", favoriteMovie);
-        getIntent().putExtra("favoriteState", favoriteMovie);                                       // store original state, need to check when activity is destroyed if need to do work (save / delete fav)
+        try {
+            return cursor.getCount() != 0;
+        } finally {
+            cursor.close();
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        saveScrollViewState(outState);
+    }
+
+    private void saveScrollViewState(Bundle outState) {
         final ScrollView scrollView =
                 (ScrollView) findViewById(R.id.activity_details_scrollview);
         final int scrollPosY = scrollView.getScrollY();
@@ -66,8 +80,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     }
 
     private void loadPoster() {
-        final Movie movie = new Movie(getApplicationContext(),
-                getIntent().getParcelableExtra(Intent.EXTRA_STREAM));
+        final Movie movie = getMovieFromIntent(getApplicationContext());
         final ImageView view = (ImageView) findViewById(R.id.movie_details_poster);
 
         Glide.with(this)
@@ -81,48 +94,86 @@ public class MovieDetailsActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {                                                 // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.movie_details, menu);
-        if(getIntent().getBooleanExtra("favoriteState", false)) {
+        if(isFavoriteMovie()) {
             final MenuItem menuItem = menu.findItem(R.id.action_favorite);
-            menuItem.setIcon(R.mipmap.ic_favorite_48dp);
+            final boolean displayToastMsg = false;
+            displayFavoriteIcon(menuItem, displayToastMsg);
         }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {                                           // Handle action bar item clicks here. The action bar will
-        int id = item.getItemId();                                                                  // automatically handle clicks on the Home/Up button, so long
-        if(item.getItemId() == R.id.action_favorite) {                                              // as you specify a parent activity in AndroidManifest.xml.
-            final boolean favoriteMovie = !getIntent().getBooleanExtra("favoriteState", false);
-            getIntent().putExtra("favoriteState", favoriteMovie);                                   // button press means favorite state has changed, update favorite flag
+        final int id = item.getItemId();                                                            // automatically handle clicks on the Home/Up button, so long
+        if(id == R.id.action_favorite) {                                                            // as you specify a parent activity in AndroidManifest.xml.
+            final boolean favoriteMovie = !isFavoriteMovie();
+            setFavoriteMovie(favoriteMovie);                                                        // button press means favorite state has changed, update favorite flag
             if(favoriteMovie) {
-                item.setIcon(R.mipmap.ic_favorite_48dp);
-                Toast.makeText(this, "Added To Favorites", Toast.LENGTH_SHORT).show();
+                final boolean displayToastMsg = true;
+                displayFavoriteIcon(item, displayToastMsg);
             } else {
-                item.setIcon(R.mipmap.ic_favorite_border_blue_48dp);
-                Toast.makeText(this, "Removed From Favorites", Toast.LENGTH_SHORT).show();
+                displayNotFavoriteIcon(item);
             }
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void displayFavoriteIcon(final MenuItem menuItem, boolean displayToastMsg) {
+        menuItem.setIcon(R.mipmap.ic_favorite_48dp);
+        if(displayToastMsg) {
+            Toast.makeText(this, "Added To Favorites", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void displayNotFavoriteIcon(final MenuItem menuItem) {
+        menuItem.setIcon(R.mipmap.ic_favorite_border_blue_48dp);
+        Toast.makeText(this, "Removed From Favorites", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onDestroy() {
         try {
-            final boolean favoriteMovie = getIntent().getBooleanExtra("favoriteState", false);
-            if(getIntent().getBooleanExtra("originalFavoriteState", false) == favoriteMovie) {      // nothing changed, don't do anything
+            final boolean favoriteMovie = isFavoriteMovie();
+            if(getOriginalFavoriteState() == favoriteMovie) {                                       // nothing changed, don't do anything
                 return;
             }
-            final Intent intent = new Intent(getBaseContext(), FavoriteService.class);
             final String command = favoriteMovie
                     ? FavoriteService.CMD_ADD_FAV
                     : FavoriteService.CMD_REM_FAV;
-            intent.putExtra(FavoriteService.INTENT_CMD_PARAM, command);
-            intent.putExtra(Intent.EXTRA_STREAM,
-                    getIntent().getParcelableExtra(Intent.EXTRA_STREAM));
-            startService(intent);
+            processFavoriteServiceCommand(command);
         } finally {
             super.onDestroy();
         }
+    }
+
+    private void processFavoriteServiceCommand(final String command) {
+        final Intent intent = new Intent(getBaseContext(), FavoriteService.class);
+        intent.putExtra(FavoriteService.INTENT_CMD_PARAM, command);
+        intent.putExtra(Intent.EXTRA_STREAM,                                                        // need to forward as it includes movie data
+                getIntent().getParcelableExtra(Intent.EXTRA_STREAM));
+        startService(intent);
+    }
+
+    private boolean getOriginalFavoriteState() {
+        return getIntent().getBooleanExtra(
+                getString(R.string.original_favorite_state_intent_param), false);
+    }
+
+    private void setOriginalFavoriteState(final boolean favoriteState) {
+        getIntent().putExtra(
+                getString(R.string.original_favorite_state_intent_param), favoriteState);
+    }
+
+    private boolean isFavoriteMovie() {
+        return getIntent().getBooleanExtra(getString(R.string.favorite_state_intent_param), false);
+    }
+
+    private void setFavoriteMovie(final boolean favoriteState) {
+        getIntent().putExtra(getString(R.string.favorite_state_intent_param), favoriteState);
+    }
+
+    private Movie getMovieFromIntent(final Context context) {
+        return new Movie(context, getIntent().getParcelableExtra(Intent.EXTRA_STREAM));
     }
 }
