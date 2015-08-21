982427bf038c9870d60ec459f468fe5d127709e4
Denny <djguitarstyle@gmail.com>
2015-08-21 16:21:36 -0700

    Generalize receivers to implement where needed

---
+++ app/src/main/java/com/example/android/popularmovies/receivers/GetReceiver.java
@@ -0,0 +1,20 @@
+package com.example.android.popularmovies.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Denny on 8/19/2015.
+ */
+public abstract class GetReceiver extends BroadcastReceiver {
+    public static final String MOVIE_DOWNLOAD_INTENT = "MOVIE_DOWNLOAD";
+    public static final String TRAILER_DOWNLOAD_INTENT = "TRAILER_DOWNLOAD";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        onDownloadComplete(context, intent);
+    }
+
+    public abstract void onDownloadComplete(Context context, Intent intent);
+}
