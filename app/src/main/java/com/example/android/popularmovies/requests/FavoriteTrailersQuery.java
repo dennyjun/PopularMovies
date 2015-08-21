659d9150ae8d4c5eb867f3ff3bc068489c7cccb6
Denny <djguitarstyle@gmail.com>
2015-08-21 16:28:37 -0700

    Add a db query to get favorite trailers

---
+++ app/src/main/java/com/example/android/popularmovies/requests/FavoriteTrailersQuery.java
@@ -0,0 +1,43 @@
+package com.example.android.popularmovies.requests;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.databases.MovieDbHelper;
+import com.example.android.popularmovies.providers.MovieContentProvider;
+
+/**
+ * Created by Denny on 8/20/2015.
+ */
+public class FavoriteTrailersQuery extends DbQuery {
+    @Override
+    public Uri getUri(Context context) {
+        return MovieContentProvider.TRAILERS_CONTENT_URI;
+    }
+
+    @Override
+    public String[] getProjection(Context context) {
+        return null;
+    }
+
+    @Override
+    public String getSelection(Context context) {
+        return MovieDbHelper.FAVORITES_TABLE_NAME +
+                context.getString(R.string.moviedb_id_param) + "=?";
+    }
+
+    @Override
+    public String[] getSelectionArgs(Context context, final Intent intent,
+                                     final ContentValues contentValues) {
+        final String movieId = intent.getStringExtra(context.getString(R.string.moviedb_id_param));
+        return new String[]{movieId};
+    }
+
+    @Override
+    public String getSortOrder(Context context) {
+        return context.getString(R.string.moviedb_trailer_name_param) + " ASC";
+    }
+}
