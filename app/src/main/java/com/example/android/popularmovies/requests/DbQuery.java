482bcb82bd9f3b0a9f703980bab1a49487ead8a8
Denny <djguitarstyle@gmail.com>
2015-08-21 16:26:13 -0700

    Add serializable base query that can be passed along

---
+++ app/src/main/java/com/example/android/popularmovies/requests/DbQuery.java
@@ -0,0 +1,36 @@
+package com.example.android.popularmovies.requests;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+
+import java.io.Serializable;
+
+/**
+ * Created by Denny on 8/20/2015.
+ */
+public abstract class DbQuery implements Serializable {
+    public static final String INTENT_NAME = DbQuery.class.getCanonicalName();
+
+    public final Cursor execute(final Context context, final Intent intent) {
+        return execute(context, intent, null);
+    }
+
+    public final Cursor execute(final Context context, final Intent intent,
+                                final ContentValues contentValues) {
+        return context.getContentResolver().query(
+                getUri(context),
+                getProjection(context),
+                getSelection(context),
+                getSelectionArgs(context, intent, contentValues),
+                getSortOrder(context));
+    }
+
+    public abstract Uri getUri(final Context context);
+    public abstract String[] getProjection(final Context context);
+    public abstract String getSelection(final Context context);
+    public abstract String[] getSelectionArgs(final Context context, final Intent intent, final ContentValues contentValues);
+    public abstract String getSortOrder(final Context context);
+}
