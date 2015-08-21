3afb0614c6d8e7441a7e98efcb85c5ac72a86377
Denny <djguitarstyle@gmail.com>
2015-08-21 16:40:34 -0700

    Add logic to handle favorite movie data updates

--- app/src/main/java/com/example/android/popularmovies/adapters/recyclerview/MoviePosterAdapter.java
+++ app/src/main/java/com/example/android/popularmovies/adapters/recyclerview/MoviePosterAdapter.java
@@ -2,8 +2,11 @@ package com.example.android.popularmovies.adapters.recyclerview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,28 +17,30 @@ import com.bumptech.glide.Glide;
 import com.example.android.popularmovies.R;
 import com.example.android.popularmovies.callbacks.OpenMovieDetailsCallback;
 import com.example.android.popularmovies.data.Movie;
-import com.example.android.popularmovies.services.GetMovieService;
+import com.example.android.popularmovies.requests.FavoriteMovieListQuery;
+import com.example.android.popularmovies.requests.FavoriteMovieQuery;
+import com.example.android.popularmovies.requests.FavoriteMovieUpdate;
+import com.example.android.popularmovies.requests.GetMovieListRequest;
+import com.example.android.popularmovies.requests.GetRequest;
+import com.example.android.popularmovies.services.GetService;
 import com.example.android.popularmovies.utils.AppUtil;
+import com.example.android.popularmovies.utils.SqlUtil;
+import com.example.android.popularmovies.utils.WebUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
 
 /**
  * Created by Denny on 8/6/2015.
  * Displays movie posters
  */
 public class MoviePosterAdapter extends BaseRecyclerAdapter<Movie> {
+    private static final String LOG_TAG = MoviePosterAdapter.class.getSimpleName();
+
     private String sortMethod = null;
     private transient OpenMovieDetailsCallback openMovieDetailsCallback;
-
-    private final transient Handler runnableHandler = new Handler();
-    private final transient Runnable getNextPage = new Runnable() {
-        @Override
-        public void run() {
-            showProgressBar();
-            final Intent msg = new Intent(getContext(), GetMovieService.class);
-            msg.putExtra(getContext().getString(R.string.moviedb_sort_by_param), sortMethod);
-            msg.putExtra(getContext().getString(R.string.moviedb_page_param), getPage());
-            getContext().startService(msg);
-        }
-    };
 
     public MoviePosterAdapter(Context context) {
         super(context);
@@ -51,8 +56,16 @@ public class MoviePosterAdapter extends BaseRecyclerAdapter<Movie> {
 
         @Override
         public void onClick(View v) {
-            final Movie movie = getItem(getAdapterPosition());
-            openMovieDetailsCallback.openMovieDetails(movie);
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    final Movie movie = getItem(getAdapterPosition());
+                    if (dbNeedsToBeUpdated(movie)) {
+                        updateItem(getAdapterPosition(), updateFavoriteMovieData(movie));
+                    }
+                    openMovieDetailsCallback.openMovieDetails(movie);
+                }
+            });
         }
     }
 
@@ -98,7 +111,38 @@ public class MoviePosterAdapter extends BaseRecyclerAdapter<Movie> {
 
         setLoading(true);
         updateSortMethod();
-        runnableHandler.post(getNextPage);
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                showProgressBar();
+                final Intent msg = new Intent(getContext(), GetService.class);
+                msg.putExtra(getContext().getString(R.string.moviedb_sort_by_param), sortMethod);
+                msg.putExtra(getContext().getString(R.string.moviedb_page_param), getPage());
+                msg.putExtra(GetRequest.class.getCanonicalName(),
+                        new GetMovieListRequest());
+                getContext().startService(msg);
+            }
+        });
+    }
+
+    public void getFavorites() {
+        if(isLoading()) {
+            return;
+        }
+
+        setLoading(true);
+        updateSortMethod();
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                showProgressBar();
+                final Intent msg = new Intent(getContext(), GetService.class);
+                msg.putExtra(FavoriteMovieListQuery.INTENT_NAME, new FavoriteMovieListQuery());
+                msg.putExtra(GetRequest.class.getCanonicalName(),
+                        new GetMovieListRequest());
+                getContext().startService(msg);
+            }
+        });
     }
 
     public boolean sortMethodChanged() {
@@ -112,5 +156,37 @@ public class MoviePosterAdapter extends BaseRecyclerAdapter<Movie> {
 
     public void setOpenMovieDetailsCallback(OpenMovieDetailsCallback openMovieDetailsCallback) {
         this.openMovieDetailsCallback = openMovieDetailsCallback;
+    }
+
+    private boolean dbNeedsToBeUpdated(final Movie movie) {
+        Cursor cursor = null;
+        try {
+            cursor = new FavoriteMovieQuery().execute(getContext(), null,
+                    movie.createContentValues(getContext()));
+            movie.setFavorite(cursor.moveToNext());
+            if(!movie.isFavorite()) {                                                               // doesn't exist in favorites table, not a favorite movie
+                return false;
+            }
+            final int index = cursor.getColumnIndex(getContext().getString(
+                    R.string.sql_timestamp_column_name));
+            final long timestamp = cursor.getLong(index);
+            final long delta = new Date().getTime() - timestamp;
+            return SqlUtil.millisToHours(delta) > 24;                                               // only need to update once in a 24 hour time period
+        } finally {
+            if(cursor != null) cursor.close();
+        }
+    }
+
+    private Movie updateFavoriteMovieData(Movie movie) {
+        final FavoriteMovieUpdate favoriteMovieUpdate = new FavoriteMovieUpdate();
+        final String jsonResult = WebUtil.get(favoriteMovieUpdate.buildDownloadUrl(getContext(),
+                movie.createContentValues(getContext())));
+        try {
+            movie = new Movie(getContext(), new JSONObject(jsonResult));
+            favoriteMovieUpdate.execute(getContext(), movie.createContentValues(getContext()));
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Unable to update favorite movie data", e);
+        }
+        return movie;
     }
 }
