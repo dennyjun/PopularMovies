dfa7044ee9941f57a9be554e04c1419cb674c91e
Denny <djguitarstyle@gmail.com>
2015-08-21 16:28:07 -0700

    Add db update to update a favorite movie

---
+++ app/src/main/java/com/example/android/popularmovies/requests/FavoriteMovieUpdate.java
@@ -0,0 +1,41 @@
+package com.example.android.popularmovies.requests;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+
+import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.providers.MovieContentProvider;
+import com.example.android.popularmovies.utils.AppUtil;
+import com.example.android.popularmovies.utils.MovieDbUtil;
+
+/**
+ * Created by Denny on 8/20/2015.
+ */
+public class FavoriteMovieUpdate extends DbUpdate {
+    @Override
+    public String buildDownloadUrl(Context context, ContentValues contentValues) {
+        final String id = contentValues.getAsString(context.getString(R.string.moviedb_id_param));
+        final Uri.Builder baseUrl = MovieDbUtil.getMovieBaseUri(context);
+        baseUrl.appendPath(id);
+        baseUrl.appendQueryParameter(context.getString(R.string.moviedb_api_key_param),
+                AppUtil.getMetaDataString(context, R.string.moviedb_api_key_meta_data));
+        return baseUrl.build().toString();
+    }
+
+    @Override
+    public Uri getUri(Context context) {
+        return MovieContentProvider.FAVORITES_CONTENT_URI;
+    }
+
+    @Override
+    public String getWhereStatement(Context context) {
+        return context.getString(R.string.moviedb_id_param) + "=?";
+    }
+
+    @Override
+    public String[] getSelectionArgs(final Context context, ContentValues contentValues) {
+        final String movieId = context.getString(R.string.moviedb_id_param);
+        return new String[]{contentValues.getAsString(movieId)};
+    }
+}
